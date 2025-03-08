a1fc139f96402b61130263f3f52069616afad7f0
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2025-03-08 12:11:21 +0800

    [WIP] adding tuntap, and starting

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cargo b
+echo $CARGO_TARGET_DIR
+# sudo setcap cap_net_admin+ep
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use tun_tap::{ Iface, Mode };
+
+fn main() -> io::Result<()>  {
+    let iface = Iface::new("tun0", Mode::Tun)?;
+    // Configure the device ‒ set IP address on it, bring it up.
+    let mut buf = vec![0; 1504]; // MTU + 4 for the header
+    loop{
+        iface.recv(&mut buf)?; // Wait until a packet arrives
+        println!("{:?}", buf);
+    }
 }
