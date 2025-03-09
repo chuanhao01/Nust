5dda3425b17d5e12e32d76e14b7565e44b8e8419
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2025-03-09 13:13:52 +0800

    [WIP] runs the tun0 interface

--- run.sh
+++ run.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
-cargo b
-echo $CARGO_TARGET_DIR
-# sudo setcap cap_net_admin+ep
+
+# Handle -d and -h
+OPTSTRING=":dh"
+DEBUG=false
+while getopts ${OPTSTRING} opt; do
+	case ${opt} in
+	d)
+		DEBUG=true
+		;;
+	h)
+        echo "Run \`./run.sh\` to run in release mode"
+        echo "Use \`-d\` to run in debug"
+		exit 1
+		;;
+	?)
+		echo "Use only -d for debug mode and -h for help"
+		exit 1
+		;;
+	esac
+done
+
+INTERFACE_NAME="tun0"
+
+BIN_NAME="nust"
+if [ "$DEBUG" = true ] ; then
+    cargo build
+    CARGO_DIR="./target/debug"
+else
+    cargo build -r
+    CARGO_DIR="./target/release"
+fi
+sudo setcap cap_net_admin+ep $CARGO_DIR/$BIN_NAME # Give perms to do network stuff
+$CARGO_DIR/$BIN_NAME & # Start running main.rs
+pid=$!
+sudo ip addr add 192.168.0.1/24 dev $INTERFACE_NAME
+sudo ip link set up dev $INTERFACE_NAME
+trap "kill $pid" INT TERM
+wait $pid
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use std::io;
 
 use tun_tap::{ Iface, Mode };
 
+const INTERFACE_NAME: &str = "tun0";
+
 fn main() -> io::Result<()>  {
-    let iface = Iface::new("tun0", Mode::Tun)?;
+    let iface = Iface::new(INTERFACE_NAME, Mode::Tun)?;
     // Configure the device ‒ set IP address on it, bring it up.
     let mut buf = vec![0; 1504]; // MTU + 4 for the header
     loop{
