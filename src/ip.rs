45ebee2b278165c50398353dbffcf920d849dd62
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2025-03-12 17:25:31 +0800

    [test] for IPPacket and IPHeader

--- src/ip.rs
+++ src/ip.rs
@@ -1,5 +1,7 @@
 use std::net::Ipv4Addr;
 
+use crate::checksum;
+
 pub struct IPPacket {
     pub header: IPHeader,
 }
@@ -14,6 +16,11 @@ impl IPPacket {
 #[derive(Debug)]
 pub struct IPPacketError {
     kind: IPPacketErrorKind,
+}
+impl IPPacketError {
+    pub fn new(kind: IPPacketErrorKind) -> Self {
+        Self { kind }
+    }
 }
 #[derive(Debug)]
 pub enum IPPacketErrorKind {
@@ -38,6 +45,42 @@ pub struct IPHeader {
     pub options: Option<Vec<u8>>,
 }
 impl IPHeader {
+    /// To create a new IPHeader given fields, calculates and sets the header_checksum for you
+    pub fn new(
+        version: u8, // 4 bits
+        ihl: u8,     // 4 bits
+        type_of_service: u8,
+        total_length: u16,
+        identification: u16,
+        flags: u8,            // 3 bits
+        fragment_offset: u16, // 13 bits
+        time_to_live: u8,
+        protocol: u8,
+        source_addr: Ipv4Addr,
+        destination_addr: Ipv4Addr,
+        options: Option<Vec<u8>>,
+    ) -> Self {
+        let mut ip_header = Self {
+            version,
+            ihl,
+            type_of_service,
+            total_length,
+            identification,
+            flags,
+            fragment_offset,
+            time_to_live,
+            protocol,
+            header_checksum: 0x0,
+            source_addr,
+            destination_addr,
+            options,
+        };
+        let header_checksum =
+            !checksum::ones_complement_sum_byte_buffer(&ip_header.to_byte_buffer()); // Taking the one's complement
+        ip_header.header_checksum = header_checksum;
+        ip_header
+    }
+    /// Parsing from raw bytes buffer
     pub fn from_byte_buffer(buf: &[u8]) -> Result<Self, IPPacketError> {
         let ihl = Self::get_ihl(buf[0]);
         let options = if ihl == 5 {
@@ -45,6 +88,9 @@ impl IPHeader {
         } else {
             Some(buf[20..].to_vec())
         };
+        if checksum::ones_complement_sum_byte_buffer(buf) != 0xFFFF {
+            return Err(IPPacketError::new(IPPacketErrorKind::IPHeaderChecksumError));
+        }
         Ok(Self {
             version: Self::get_version(buf[0]),
             ihl,
@@ -63,7 +109,20 @@ impl IPHeader {
     }
     /// Creates the byte buffer using the values in the header
     fn to_byte_buffer(&self) -> Vec<u8> {
-        [((self.version << 4) + self.ihl)].to_vec()
+        let mut buf = [((self.version << 4) + self.ihl), self.type_of_service].to_vec();
+        buf.append(&mut self.total_length.to_be_bytes().to_vec());
+        buf.append(&mut self.identification.to_be_bytes().to_vec());
+        buf.push((self.flags << 5) + (self.fragment_offset >> 8) as u8);
+        buf.push((self.fragment_offset & 0xFF) as u8);
+        buf.push(self.time_to_live);
+        buf.push(self.protocol);
+        buf.append(&mut self.header_checksum.to_be_bytes().to_vec());
+        buf.append(&mut self.source_addr.to_bits().to_be_bytes().to_vec());
+        buf.append(&mut self.destination_addr.to_bits().to_be_bytes().to_vec());
+        if let Some(options) = &self.options {
+            buf.append(&mut options.clone());
+        }
+        buf
     }
 
     fn get_version(x: u8) -> u8 {
@@ -98,6 +157,35 @@ mod tests {
             ];
             let ip_header = IPHeader::from_byte_buffer(&buf).unwrap();
             assert_eq!(ip_header.version, 0x4);
+            assert_eq!(ip_header.ihl, 0x5);
+            assert_eq!(ip_header.type_of_service, 0x0);
+            assert_eq!(ip_header.total_length, 0x54);
+            assert_eq!(ip_header.identification, 0x1b0b);
+            assert_eq!(ip_header.flags, 0b010);
+            assert_eq!(ip_header.fragment_offset, 0x0);
+            assert_eq!(ip_header.time_to_live, 0x40);
+            assert_eq!(ip_header.protocol, 0x1);
+            assert_eq!(ip_header.header_checksum, 0x9e4a);
+            assert_eq!(ip_header.source_addr, Ipv4Addr::from_bits(0xc0a80001));
+            assert_eq!(ip_header.destination_addr, Ipv4Addr::from_bits(0xc0a80002));
+            assert!(ip_header.options.is_none());
+            let ip_header_buf = ip_header.to_byte_buffer();
+            assert_eq!(ip_header_buf, buf);
+            let new_ip_header = IPHeader::new(
+                ip_header.version,
+                ip_header.ihl,
+                ip_header.type_of_service,
+                ip_header.total_length,
+                ip_header.identification,
+                ip_header.flags,
+                ip_header.fragment_offset,
+                ip_header.time_to_live,
+                ip_header.protocol,
+                ip_header.source_addr,
+                ip_header.destination_addr,
+                ip_header.options,
+            );
+            assert_eq!(new_ip_header.header_checksum, ip_header.header_checksum);
         }
     }
 }
