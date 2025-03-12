e53169cb4b6722a38e1d236fd14dfa4263785bf9
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2025-03-12 17:04:51 +0800

    [test] for checksum one's complement

--- src/checksum.rs
+++ src/checksum.rs
@@ -1,3 +1,5 @@
+/// Calculates the u16 one's complement sum of the entire buffer
+/// Padding odd length byte with u8 0x0 to the right
 pub fn ones_complement_sum_byte_buffer(buf: &[u8]) -> u16 {
     let mut sum: u32 = 0;
     assert!(!buf.is_empty()); // Should panic if buffer passed in is empty, might change in the future
@@ -14,5 +16,24 @@ pub fn ones_complement_sum_byte_buffer(buf: &[u8]) -> u16 {
     while sum >> 16 != 0 {
         sum = (sum & 0xFFFF) + (sum >> 16);
     }
-    !(sum as u16) // one's complement
+    sum as u16 // one's complement sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod ones_complement_sum_byte_buffer_tests {
+        use super::*;
+        #[test]
+        fn single_carry_over() {
+            let buf: [u8; 4] = [0x80, 0x0, 0x80, 0x0];
+            assert_eq!(0x1, ones_complement_sum_byte_buffer(&buf));
+        }
+        #[test]
+        fn double_carry_over() {
+            let buf: [u8; 6] = [0x80, 0x0, 0x80, 0x0, 0xFF, 0xFF];
+            assert_eq!(0x1, ones_complement_sum_byte_buffer(&buf));
+        }
+    }
 }
